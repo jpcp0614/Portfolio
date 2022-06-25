035132e79dddd344e2b55f033a239ab1473955a2
jpcp0614 <jpcp0614@gmail.com>
2022-06-25 19:35:16 -0300

    :sparkles: feat: Organization of the components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
+import About from './components/about/About';
+import Contact from './components/contact/Contact';
+import Footer from './components/footer/Footer';
+import Header from './components/header/Header';
+import Nav from './components/nav/Nav';
+import Portfolio from './components/portfolio/Portfolio';
 
-function App() {
-  return (
-    <div>App</div>
-  )
-}
+const App = () => {
+	return (
+    <>
+      <Header />
+      <Nav />
+      <About />
+      <Portfolio />
+      <Contact />
+      <Footer />
+    </>
+  );
+};
 
 export default App;
