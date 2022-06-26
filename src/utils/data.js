78df402716f4cc63a8fede16131da48b50e70c82
jpcp0614 <jpcp0614@gmail.com>
2022-06-26 01:52:04 -0300

    :sparkles: feat: add data Projects

---
+++ src/utils/data.js
@@ -0,0 +1,53 @@
+import IMG1 from '../assets/portfolio1.jpg';
+import IMG2 from '../assets/portfolio2.jpg';
+import IMG3 from '../assets/portfolio3.jpg';
+import IMG4 from '../assets/portfolio4.jpg';
+import IMG5 from '../assets/portfolio5.png';
+import IMG6 from '../assets/portfolio6.jpg';
+
+const data = [
+  {
+    id: 1,
+    title: "Calculator",
+    github: "https://github.com/jpcp0614/Project-Calculator",
+    demo: "https://calculator-sigma-three.vercel.app/",
+		img: IMG1
+  },
+  {
+    id: 2,
+    title: "Project 2",
+    github: "https://github.com/jpcp0614",
+    demo: "https://www.google.com/",
+		img: IMG2
+  },
+  {
+		id: 3,
+		title: "Project 3",
+		github: "https://github.com/jpcp0614",
+    demo: "https://www.google.com/",
+		img: IMG3
+  },
+	{
+		id: 4,
+		title: "Project 4",
+		github: "https://github.com/jpcp0614",
+    demo: "https://www.google.com/",
+		img: IMG4
+  },
+	{
+		id: 5,
+		title: "Project 5",
+		github: "https://github.com/jpcp0614",
+    demo: "https://www.google.com/",
+		img: IMG5
+  },
+	{
+		id: 6,
+		title: "Project 6",
+		github: "https://github.com/jpcp0614",
+    demo: "https://www.google.com/",
+		img: IMG6
+  }
+]
+
+export default data;
