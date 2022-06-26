70091b69f8c1a29259bba5db4d945e77ab34e104
jpcp0614 <jpcp0614@gmail.com>
2022-06-25 23:22:51 -0300

    :sparkles: feat: add id

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,7 +6,7 @@ import './Header.css';
 
 const Header = () => {
 	return (
-		<header>
+		<header id="header">
 			<div className="container header__container">
 				<h5>Olá, me chamo</h5>
 				<h1>João Paulo Pereira</h1>
